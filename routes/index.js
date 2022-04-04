5d6d71805e73259b6524014d25228b6658b0b2fb
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2022-04-04 10:57:42 +0200

    added auth token

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const router = express.Router();
 const bcrypt = require('bcrypt')
 const moment = require('moment')
+const crypto = require('crypto')
 
 //connexion bdd sql
 const db = mysql.createConnection({
@@ -27,17 +28,30 @@ router.post('/login', (req, res)=> {
         });
     } else {
         db.query("SELECT * FROM user WHERE email = ?", [email], (err, result) => {
-
                 if (err) {
                     res.send({err: err});
                 }
                 if (result.length > 0) {
+
                     let stockedPassword = result[0]['password']
+
                     bcrypt.compare(password, stockedPassword, (err, isMatch) =>{
+
                         if(isMatch){
-                            res.json({
-                                status: "SUCCESS",
-                                result: result
+
+                            //Ici est généré un nouveau token crypté de connexion, stocké dans une variable
+                            let newConnectionToken = crypto.randomBytes(64).toString('hex');
+
+                            //A chaque connexion, le token est mis à jour dans la db, car si il reste tout le temps le même un attaquant peut voler le token de quelqu'un et le réutiliser
+                            db.query("UPDATE user SET loginToken = ? WHERE id = ?", [newConnectionToken, result[0]["id"]], (err, data) =>{
+                                if(err) throw err
+
+                                //Ici on change le token dans la réponse, car la variable "result" contient l'ancien token, et il faut récupérer le nouveau dans la base de données.
+                                result[0]['loginToken'] = newConnectionToken
+                                res.json({
+                                    status: "SUCCESS",
+                                    result: result
+                                })
                             })
                         } else {
                             res.json({
@@ -90,8 +104,6 @@ router.post('/register', (req, res) =>{
     }
         bcrypt.genSalt(10, function(err, salt) {
             bcrypt.hash(password, salt, function(err, hash) {
-
-
                 db.query(
                     "INSERT INTO user (email, password, first_name, last_name, register_date, responsable, admin, suspendu, dateDebut, dateFin) VALUES (?)",
                     [[mailAddress, hash, firstName, lastName, moment(Date.now()).format('YYYY-MM-DD'), 0, 0, 0, null, null]],
